821d7702d8061ea59387eebcb4e79fa33539e8ef
DESKTOP-HVP6MPC\Chris <chris@thecarmans.com>
2017-05-09 10:59:06 +0200

    played around with python and numpy in anaconda

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy import special, optimize
+import matplotlib.pyplot as plt
+
+print('Hello World')
+
+#Just added a matrix and printet it, arrange creates 15 long vector, and then reshapes it into a matrix
+a = np.arange(15).reshape(3,5)
+print(a)
+
+#created a vector with evenly spaced numbers from -100 to 100 spaced with 1
+x = np.linspace(-100,1,100)
+print(x)
+
